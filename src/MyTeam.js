0e20dedbfc94e6cabe95243c9621a665f85947bd
Jared Lindolent <jared.lindolent@standardbank.co.za>
2021-01-12 18:01:45 +0200

    added search function and ui

--- src/MyTeam.js
+++ src/MyTeam.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import './Style.css';
+import './style.css';
 import Search from './Search';
 
 const MyTeam = () => {
   const [team, setTeam] = useState([]);
+  const [searchField, setSearchField] = useState([]);
+  const [members, setMembers] = useState([]);
+  const [showCard, setShowCard] = useState(false);
 
   const loadData = async () => {
     const endpoint = 'https://jsonplaceholder.typicode.com/users';
@@ -16,15 +19,49 @@ const MyTeam = () => {
 
   useEffect(() => {
     loadData();
-  }, [])
+  }, []);
 
   useEffect(() => {
+    if (members.length > 0) {
+      setShowCard(true);
+    }
+  }, [members]);
 
-  }, [team])
+  const onSearchChangeHandler = (value) => {
+    setSearchField([]);
+    setSearchField(value);
+  }
+
+  const onSubmitHandler = () => {
+    setMembers([]);
+    let found = team.find(item => item.name === searchField);
+    if (found !== undefined) {
+      setMembers(members => [...members, found]);
+    } else { setShowCard(false) }
+  }
 
   return (
     <div className="App">
-      <Search />
+      <Search onSearchChanged={onSearchChangeHandler} props={team} />
+      <button type="submit" onClick={onSubmitHandler}>Submit</button>
+
+      {showCard ?
+        <>
+          {members.map(item => (
+            <div key={item.id} className="card">
+              <div className="container">
+                <h4><b>{item.name}</b></h4>
+                <p><b>E-mail:</b> {item.email}</p>
+                <p><b>Address:</b> {item.address.suite} {item.address.street}, {item.address.city}, {item.address.zipcode}</p>
+                <p><b>Phone:</b> {item.phone}</p>
+                <p><b>Website:</b> {item.website}</p>
+                <p><b>Company Name:</b> {item.company.name}</p>
+                <p><b>Catch phrase:</b> {item.company.catchPhrase}</p>
+              </div>
+            </div>
+          ))}
+        </>
+        : null}
     </div>
   );
 }
