7f5d28f0ed404017eae2da646e8711d69e4a3909
Jared Lindolent <jared.lindolent@standardbank.co.za>
2021-01-12 18:01:23 +0200

    added search field function

--- src/Search.js
+++ src/Search.js
@@ -1,35 +1,26 @@
-import React, { useState } from 'react';
-import './Style.css';
+import React, { useState, useEffect } from 'react';
+import './style.css';
 
-const Search = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+const Search = (props) => {
+    const [searchField, setSearchField] = useState('');
+
+    useEffect(() => {
+        props.onSearchChanged(searchField);
+    }, [searchField])
 
     const updateSearchData = (e) => {
-        if (e.target.name === "firstName") { setName(e.target.value) }
-        if (e.target.name === "email") { setEmail(e.target.email) }
-    }
-
-    const onSubmitHandler = () => {
+        setSearchField(e.target.value)
     }
 
     return (
         <form>
             <input
-                value={name}
+                value={searchField}
                 onChange={e => updateSearchData(e)}
-                placeholder="First name"
+                placeholder="Search by name or e-mail"
                 type="text"
-                name="firstName"
+                name="searchField"
             />
-            <input
-                value={email}
-                onChange={e => updateSearchData(e)}
-                placeholder="E-mail"
-                type="email"
-                name="email"
-            />
-            <button type="submit" onClick={onSubmitHandler}>Submit</button>
         </form>
     )
 }
